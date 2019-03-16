af819c5352ee6656e8f83b099da97adea1e1aaf4
Aaron Westre <aaron@artificialnatures.com>
2019-03-16 11:39:01 -0500

    added video feed

---
+++ unity/PeoplePixels/Assets/VideoScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class VideoScreen : MonoBehaviour
+{
+    void Start()
+    {
+        var videoTexture = new WebCamTexture();
+        GetComponent<Renderer>().material.mainTexture = videoTexture;
+        videoTexture.Play();
+    }
+
+    void Update()
+    {
+        
+    }
+}
